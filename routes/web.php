a64f7f0f08980e63e179263dbf264f87c969d83b
Yunila Putmasari <nilaputma26@gmail.com>
2024-02-20 10:01:19 +0700

    basic routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2241720062 - Yunila Putmasari';
+});
